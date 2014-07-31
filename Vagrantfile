966dcfd31492fa16ca9e3b2a51dbface6e91d09a
John Mark Schofield <jms@schof.org>
2014-07-31 16:45:04 -0700

    Initial version of a mostly working Vagrant install script. NOT FULLY TESTED. At this time the script has gotten through installing rabbit, but has not completed a run. Committing unfinished work so Miguel can take a look.

--- Vagrantfile
+++ Vagrantfile
@@ -1,14 +1,25 @@
 # -*- mode: ruby -*-
 # vi: set ft=ruby :
 
+$proxyconfig = <<SCRIPT
+echo 'Acquire::http::Proxy "http://10.5.5.5:3142";' > /etc/apt/apt.conf.d/01proxy
+SCRIPT
+
+
+# Script requires that https://github.com/johnmarkschofield/vagrant-apt-cacher be installed and running.
+
+
 VAGRANTFILE_API_VERSION = "2"
 
 Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
+    config.vm.provision "shell", inline: $proxyconfig
     config.vm.box_url = "https://cloud-images.ubuntu.com/vagrant/trusty/current/trusty-server-cloudimg-amd64-vagrant-disk1.box"
     config.vm.box = "trusty64"
     config.vm.hostname = "infra1"
+    config.ssh.forward_agent = true
     config.vm.network "private_network", ip: "10.0.0.11"
     config.vm.network "private_network", ip: "10.1.0.11", auto_config: false
+    config.vm.network "private_network", ip: "10.5.5.16" # required for vagrant-apt-cacher
     config.vm.provider "virtualbox" do |vb|
         vb.customize ["modifyvm", :id, "--memory", "4096"]
         vb.customize ["modifyvm", :id, "--nicpromisc3", "allow-all"]
@@ -22,4 +33,5 @@ Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
             vb.customize ['storageattach', :id, '--storagectl', 'SATAController', '--port', 2, '--device', 0, '--type', 'hdd', '--medium', './sdc.vdi']
         end
     end
+    #config.vm.provision "shell", path: "install.sh"
 end
--- install.sh
+++ install.sh
@@ -1,13 +1,47 @@
 #!/bin/bash
-cd ~/
 
-# base dependencies
-apt-get update && apt-get dist-upgrade -y
-apt-get purge -y nano
-apt-get install -y aptitude bridge-utils build-essential git python-dev python-pip ssh sudo vim lsof tcpdump
+if [ `id -u` -ne 0 ]; then
+  echo "This script must be run as root."
+  exit 1
+fi
 
-# lvm
-apt-get install -y lvm2
+set -e
+set -o pipefail
+set -x
+
+GIT_URL=https://github.com/rcbops/ansible-lxc-rpc.git
+GIT_BRANCH=development
+
+
+export DEBIAN_FRONTEND=noninteractive
+apt-get update
+apt-get install -q -y update-notifier-common
+apt-get autoremove -y
+apt-get dist-upgrade -q -y
+test -e /var/run/reboot-required && shutdown -h now || true
+
+# Disk stuff
+apt-get -q -y install lvm2 parted
+
+# Network stuff
+apt-get -q -y install bridge-utils
+
+# Build Requirements
+apt-get -q -y install git aptitude python-dev
+
+# Not-crappy editors + tools
+apt-get -q -y install emacs24-nox vim tmux
+
+# Get modern pip
+apt-get -q -y install python-setuptools
+easy_install pip
+
+
+# Install ansible
+pip install ansible==1.6.6
+
+
+# Configure Disks
 parted -s /dev/sdb mktable gpt
 parted -s /dev/sdc mktable gpt
 parted -s /dev/sdb mkpart lvm 0% 100%
@@ -17,35 +51,36 @@ pvcreate /dev/sdc1
 vgcreate lxc /dev/sdb1
 vgcreate cinder-volumes /dev/sdc1
 
+# Don't think any of this is necessary
 # LXC
-add-apt-repository -y ppa:ubuntu-lxc/stable
-apt-get update
-apt-get install -y lxc python3-lxc lxc-templates liblxc1
+# add-apt-repository -y ppa:ubuntu-lxc/stable
+# apt-get update
+# apt-get install -y lxc python3-lxc lxc-templates liblxc1
 
-git clone https://github.com/cloudnull/lxc_defiant
-cp lxc_defiant/lxc-defiant.py /usr/share/lxc/templates/lxc-defiant
-chmod +x /usr/share/lxc/templates/lxc-defiant
-cp lxc_defiant/defiant.common.conf /usr/share/lxc/config/defiant.common.conf
+# git clone https://github.com/cloudnull/lxc_defiant
+# cp lxc_defiant/lxc-defiant.py /usr/share/lxc/templates/lxc-defiant
+# chmod +x /usr/share/lxc/templates/lxc-defiant
+# cp lxc_defiant/defiant.common.conf /usr/share/lxc/config/defiant.common.conf
 
 echo net.ipv4.ip_forward=1 >> /etc/sysctl.conf
 sysctl -w net.ipv4.ip_forward=1
 
-cat > /etc/lxc/lxc-defiant.conf <<EOF
-lxc.start.auto = 1
-lxc.group = onboot
+# cat > /etc/lxc/lxc-defiant.conf <<EOF
+# lxc.start.auto = 1
+# lxc.group = onboot
 
-# Configures the default LXC network
-lxc.network.type=veth
-lxc.network.name=eth0
-lxc.network.link=lxcbr0
-lxc.network.flags=up
+# # Configures the default LXC network
+# lxc.network.type=veth
+# lxc.network.name=eth0
+# lxc.network.link=lxcbr0
+# lxc.network.flags=up
 
-# Creates a veth pair within the container
-lxc.network.type = veth
-lxc.network.link = br-mgmt
-lxc.network.name = eth1
-lxc.network.flags = up
-EOF
+# # Creates a veth pair within the container
+# lxc.network.type = veth
+# lxc.network.link = br-mgmt
+# lxc.network.name = eth1
+# lxc.network.flags = up
+# EOF
 
 # networking
 brctl addbr br-mgmt
@@ -67,17 +102,26 @@ ifup eth2
 ifdown br-mgmt
 ifup br-mgmt
 
-# ssh keys
-ssh-keygen -f ~/.ssh/id_rsa -N ""
-cat ~/.ssh/id_rsa.pub >> /root/.ssh/authorized_keys
-chown vagrant:vagrant ~/.ssh/id_rsa*
-cp ~/.ssh/id_rsa* /root/.ssh/
-service ssh restart
 
-# ansible playbooks setup
-git clone https://github.com/rcbops/ansible-lxc-rpc.git
-pip install -r ansible-lxc-rpc/requirements.txt
-cp -R ansible-lxc-rpc/etc/rpc_deploy/ /etc/rpc_deploy
+
+rm -rf /opt/ansible-lxc-rpc
+mkdir -p /opt
+git clone $GIT_URL -b $GIT_BRANCH /opt/ansible-lxc-rpc
+
+pip install --upgrade -r /opt/ansible-lxc-rpc/requirements.txt
+
+cp -R /opt/ansible-lxc-rpc/etc/rpc_deploy /etc/rpc_deploy
+
+rm -f /root/.ssh/id_rsa
+rm -rf /root/.ssh/id_rsa.pub
+ssh-keygen -t rsa -f /root/.ssh/id_rsa -N ''
+cat ~/.ssh/id_rsa.pub >> ~/.ssh/authorized_keys
+
+# Not necessary because d34dh0r53 made these parameters auto-configuring
+# sed -i 's/^elasticsearch_heap:.*/elasticsearch_heap: 1g/g' /opt/ansible-lxc-rpc/rpc_deployment/inventory/group_vars/elasticsearch.yml
+# sed -i 's/^logstash_heap:.*/logstash_heap: 1g/g' /opt/ansible-lxc-rpc/rpc_deployment/inventory/group_vars/logstash.yml
+
+
 cat <<EOF >/etc/rpc_deploy/rpc_user_config.yml
 ---
 cidr: 10.1.0.0/24
@@ -106,24 +150,11 @@ haproxy_hosts:
   infra1:
     ip: 10.1.0.11
 EOF
-cd ansible-lxc-rpc/rpc_deployment/
+
+cd /opt/ansible-lxc-rpc/rpc_deployment/
 find . -name "*.yml" -exec sed -i "s/container_lvm_fssize: 5G/container_lvm_fssize: 2G/g" '{}' \;
-sed -i "s/lb_vip_address: 127.0.0.1/lb_vip_address: 10.1.0.11/" vars/user_variables.yml
+sed -i "s/^lb_vip_address:.*/lb_vip_address: 10.1.0.11/" /opt/ansible-lxc-rpc/rpc_deployment/vars/user_variables.yml
 
-# setup playbooks
-ansible-playbook -e @vars/user_variables.yml playbooks/setup/all-the-setup-things.yml
-if [[ $? -ne 0 ]]; then
-    exit $?
-fi
 
-# infrastructure playbooks
-ansible-playbook -e @vars/user_variables.yml playbooks/infrastructure/all-the-infrastructure-things.yml
-if [[ $? -ne 0 ]]; then
-    exit $?
-fi
+/bin/bash /vagrant/install_openstack.bash
 
-# openstack playbooks
-ansible-playbook -e @vars/user_variables.yml playbooks/openstack/all-the-openstack-things.yml
-if [[ $? -ne 0 ]]; then
-    exit $?
-fi
