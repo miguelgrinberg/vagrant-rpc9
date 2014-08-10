29bdf5066e8a4ab5e9903d20274ed2d0c7805963
Miguel Grinberg <miguelgrinberg50@gmail.com>
2014-08-09 19:37:29 -0700

    added bridges and ansible retries

--- Vagrantfile
+++ Vagrantfile
@@ -7,11 +7,12 @@ Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
     config.vm.box_url = "https://cloud-images.ubuntu.com/vagrant/trusty/current/trusty-server-cloudimg-amd64-vagrant-disk1.box"
     config.vm.box = "trusty64"
     config.vm.hostname = "infra1"
-    config.vm.network "private_network", ip: "10.0.0.11"
+    config.vm.network "private_network", ip: "10.0.0.11", auto_config: false
     config.vm.network "private_network", ip: "10.1.0.11", auto_config: false
+    config.vm.network "private_network", ip: "10.2.0.11", auto_config: false
     config.vm.provider "virtualbox" do |vb|
         vb.customize ["modifyvm", :id, "--memory", "4096"]
-        vb.customize ["modifyvm", :id, "--nicpromisc3", "allow-all"]
+        vb.customize ["modifyvm", :id, "--nicpromisc4", "allow-all"]
 
         unless File.exist?('./sdb.vdi')
             vb.customize ['createhd', '--filename', './sdb.vdi', '--size', 50 * 1024]
--- install.sh
+++ install.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
-cd ~/
+set -e
+if [ `id -u` -ne 0 ]; then
+  echo "This script must be run as root."
+    exit 1
+fi
+cd /root
 
 # base dependencies
-apt-get update && apt-get dist-upgrade -y
-apt-get purge -y nano
-apt-get install -y aptitude bridge-utils build-essential git python-dev python-pip ssh sudo vim lsof tcpdump
+apt-get update
+apt-get dist-upgrade -y
+apt-get install -y aptitude bridge-utils lxc lvm2 build-essential git python-dev python-pip
 
 # lvm
-apt-get install -y lvm2
 parted -s /dev/sdb mktable gpt
 parted -s /dev/sdc mktable gpt
 parted -s /dev/sdb mkpart lvm 0% 100%
@@ -17,62 +21,49 @@ pvcreate /dev/sdc1
 vgcreate lxc /dev/sdb1
 vgcreate cinder-volumes /dev/sdc1
 
-# LXC
-add-apt-repository -y ppa:ubuntu-lxc/stable
-apt-get update
-apt-get install -y lxc python3-lxc lxc-templates liblxc1
-
-git clone https://github.com/cloudnull/lxc_defiant
-cp lxc_defiant/lxc-defiant.py /usr/share/lxc/templates/lxc-defiant
-chmod +x /usr/share/lxc/templates/lxc-defiant
-cp lxc_defiant/defiant.common.conf /usr/share/lxc/config/defiant.common.conf
-
+# networking
 echo net.ipv4.ip_forward=1 >> /etc/sysctl.conf
 sysctl -w net.ipv4.ip_forward=1
 
-cat > /etc/lxc/lxc-defiant.conf <<EOF
-lxc.start.auto = 1
-lxc.group = onboot
-
-# Configures the default LXC network
-lxc.network.type=veth
-lxc.network.name=eth0
-lxc.network.link=lxcbr0
-lxc.network.flags=up
-
-# Creates a veth pair within the container
-lxc.network.type = veth
-lxc.network.link = br-mgmt
-lxc.network.name = eth1
-lxc.network.flags = up
-EOF
-
-# networking
-brctl addbr br-mgmt
-cat << EOF > /etc/network/interfaces.d/eth2.cfg
-auto eth2
-iface eth2 inet manual
+cat << EOF >> /etc/network/interfaces
+# br-mgmt on eth1
+auto eth1
+iface eth1 inet manual
 auto br-mgmt
 iface br-mgmt inet static
+        address 10.0.0.11
+        netmask 255.255.255.0
+        bridge_ports eth1
+
+# br-vmnet on eth2
+auto eth2
+iface eth2 inet manual
+auto br-vmnet
+iface br-vmnet inet static
         address 10.1.0.11
         netmask 255.255.255.0
         bridge_ports eth2
-        bridge_stp off
-        bridge_fd 0
-        bridge_maxwait 0
+
+# br-ext on eth3
+auto eth3
+iface eth3 inet manual
+auto br-ext
+iface br-ext inet static
+        address 10.3.0.11
+        netmask 255.255.255.0
+        bridge_ports eth3
         dns-nameservers 8.8.8.8 8.8.4.4
 EOF
-ifdown eth2
+ifup eth1
 ifup eth2
-ifdown br-mgmt
-ifup br-mgmt
+ifup eth3
+brctl addbr br-mgmt
+brctl addbr br-vmnet
+brctl addbr br-ext
 
 # ssh keys
-ssh-keygen -f ~/.ssh/id_rsa -N ""
-cat ~/.ssh/id_rsa.pub >> /root/.ssh/authorized_keys
-chown vagrant:vagrant ~/.ssh/id_rsa*
-cp ~/.ssh/id_rsa* /root/.ssh/
-service ssh restart
+ssh-keygen -f /root/.ssh/id_rsa -N ""
+cat /root/.ssh/id_rsa.pub >> /root/.ssh/authorized_keys
 
 # ansible playbooks setup
 git clone https://github.com/rcbops/ansible-lxc-rpc.git
@@ -80,50 +71,80 @@ pip install -r ansible-lxc-rpc/requirements.txt
 cp -R ansible-lxc-rpc/etc/rpc_deploy/ /etc/rpc_deploy
 cat <<EOF >/etc/rpc_deploy/rpc_user_config.yml
 ---
-cidr: 10.1.0.0/24
+mgmt_cidr: 10.0.0.0/24
+vmnet_cidr: 10.1.0.0/24
+
+global_overrides:
+  internal_lb_vip_address: 10.0.0.11
+  external_lb_vip_address: 10.2.0.11
 
 infra_hosts:
   infra1:
-    ip: 10.1.0.11
+    ip: 10.0.0.11
 
 compute_hosts:
   infra1:
-    ip: 10.1.0.11
+    ip: 10.0.0.11
 
 storage_hosts:
   infra1:
-    ip: 10.1.0.11
+    ip: 10.0.0.11
 
 log_hosts:
   infra1:
-    ip: 10.1.0.11
+    ip: 10.0.0.11
 
 network_hosts:
   infra1:
-    ip: 10.1.0.11
+    ip: 10.0.0.11
 
 haproxy_hosts:
   infra1:
-    ip: 10.1.0.11
+    ip: 10.0.0.11
 EOF
 cd ansible-lxc-rpc/rpc_deployment/
 find . -name "*.yml" -exec sed -i "s/container_lvm_fssize: 5G/container_lvm_fssize: 2G/g" '{}' \;
-sed -i "s/lb_vip_address: 127.0.0.1/lb_vip_address: 10.1.0.11/" vars/user_variables.yml
 
-# setup playbooks
-ansible-playbook -e @vars/user_variables.yml playbooks/setup/all-the-setup-things.yml
-if [[ $? -ne 0 ]]; then
-    exit $?
-fi
+exit 0
+run_playbook()
+{
+    ATTEMPT=1
+    VERBOSE=""
+    while ! ansible-playbook $VERBOSE -e @vars/user_variables.yml playbooks/$1; do 
+        if [ $ATTEMPT -ge $RETRIES ]; then
+            exit 1
+        fi
+        ATTEMPT=$((ATTEMPT+1))
+        sleep 10
+        VERBOSE=-vvv
+    done
+}
 
-# infrastructure playbooks
-ansible-playbook -e @vars/user_variables.yml playbooks/infrastructure/all-the-infrastructure-things.yml
-if [[ $? -ne 0 ]]; then
-    exit $?
-fi
+run_playbook setup/host-setup.yml
+run_playbook setup/build-containers.yml
+run_playbook setup/restart-containers.yml
+run_playbook setup/it-puts-common-bits-on-disk.yml
 
-# openstack playbooks
-ansible-playbook -e @vars/user_variables.yml playbooks/openstack/all-the-openstack-things.yml
-if [[ $? -ne 0 ]]; then
-    exit $?
-fi
+run_playbook infrastructure/galera-install.yml
+run_playbook infrastructure/memcached-install.yml
+run_playbook infrastructure/rabbit-install.yml
+run_playbook infrastructure/rsyslog-install.yml
+run_playbook infrastructure/elasticsearch-install.yml
+run_playbook infrastructure/logstash-install.yml
+run_playbook infrastructure/kibana-install.yml
+run_playbook infrastructure/rsyslog-config.yml
+run_playbook infrastructure/es2unix-install.yml
+run_playbook infrastructure/haproxy-install.yml
+
+run_playbook openstack/utility.yml
+run_playbook openstack/it-puts-openstack-bits-on-disk.yml
+run_playbook openstack/keystone.yml
+run_playbook openstack/keystone-add-all-services.yml
+run_playbook openstack/keystone-add-users.yml
+run_playbook openstack/glance-all.yml
+run_playbook openstack/heat-all.yml
+run_playbook openstack/nova-all.yml
+run_playbook openstack/neutron-all.yml
+run_playbook openstack/neutron-all.yml
+run_playbook openstack/cinder-all.yml
+run_playbook openstack/horizon.yml
